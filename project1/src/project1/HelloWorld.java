f9631462a0209c8a2a6e14c8630aae227ed5bc58
19jy0116 <19jy0116@PC14.jynet2.jec.ac.jp>
2020-10-26 14:24:10 +0900

    mod HelloをHelloWorldに変更

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,8 +3,7 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloWorld");
 
 	}
 
